ae47665d091650e60d0b9d1f75aa03d0970e0aff
Samuel Gracia <89318578+Garcia-Sam@users.noreply.github.com>
2023-10-11 17:31:08 -0300

    Update script-crud.js and style.css

--- Fokus-projeto/js/script-crud.js
+++ Fokus-projeto/js/script-crud.js
@@ -1,4 +1,9 @@
 const taskListContainer = document.querySelector(".app__section-task-list")
+//Add Tarefas
+const formTask = document.querySelector('.app__form-add-task')
+const toggleFormTaskBtn = document.querySelector('.app__button--add-task')
+const formLabel = document.querySelector('.app__form-label')
+
 const taskIconSvg = `<svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24"
 fill="none" xmlns="http://www.w3.org/2000/svg">
 <circle cx="12" cy="12" r="12" fill="#FFF" />
@@ -34,7 +39,13 @@ function createTask (tarefa) {
 
     return li
 }
+
 tarefas.forEach(task => { 
     const taskItem = createTask(task)
     taskListContainer.appendChild(taskItem)
+})
+
+toggleFormTaskBtn.addEventListener('click', () => {
+    formLabel.textContent = 'Adicionando tarefa'
+    formTask.classList.toggle('hidden')
 })
